584a35d4f52b890d83386cfead7cf01afeb9e398
Oleksii Mosunov <oleksiimo@backbase.com>
2025-12-03 15:50:12 +0100

    Initial Commit

---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Oleksii Mosunov on 03/12/2025.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
